b1b69ec4a32caf8c7482e0eaa2cb25d081c62c5c
German Kasner <89586583+Axelchannel@users.noreply.github.com>
2022-02-26 19:40:41 +0300

    Delete operator.py

--- operator.py
+++
@@ -1,6 +0,0 @@
-import arithmetic.Dif_mod as div
-import arithmetic.Mult_mod as mult
-import arithmetic.Sub_mod as sub
-import arithmetic.Sum_mod as sum
-
-dict_ar = {'/': div, '*': mult, '-': sub, '+': sum}
